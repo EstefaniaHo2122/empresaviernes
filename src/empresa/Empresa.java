fa5111ae98a0ed0537182b24b739852155004a13
EstefaniaHo2122 <estefania.ho2122@gmail.com>
2021-09-17 08:59:44 +0200

    se crea menu

--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -5,7 +5,9 @@
  */
 package empresa;
 
-/**
+import java.util.Scanner;
+
+ /**
  *
  * @author Stefania
  */
@@ -15,7 +17,22 @@ public class Empresa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner entrada = new Scanner(System.int);
+        int opcion;
+        do{
+            System.out.println("Bienvenidos");
+            System.out.println("******************");
+            System.out.println("1. Agregar Empleado");
+            System.out.println("2. Mostrar Empleado");
+            System.out.println("3. Mostrar Empleado");
+            System.out.println("4. Salir");
+            System.out.println("");
+            
+            System.out.println("Digite una opcion: ");
+            opcion=entrada.nextInt();
+            
+        }while(opcion!=4);
     }
     
 }
